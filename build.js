8f28acd2e3fd53d7b78b3d920bfbeae19711c73b
wetternest <wetternest@gmail.com>
2015-08-25 21:02:28 +0200

    config-file for optimization with r.js (http://requirejs.org/docs/optimization.html) usage: node r.js -o build.js (r.js not included)

---
+++ build.js
@@ -0,0 +1,11 @@
+({
+    baseUrl: ".",
+    //optimize: 'none',
+    //optimize: 'uglify',
+    optimize: 'uglify2',
+    paths: {
+        angular: "empty:",
+    },
+    name: "angular-bootstrap-contextmenu",
+    out: "angular-bootstrap-contextmenu.min.js"
+})
